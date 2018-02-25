449dfbf7653b512d86f71ff3b61a4b18b097ef99
aqie123 <2924811900@qq.com>
2018-02-25 16:12:25 +0800

    自定义队列生成迷宫添加 迷雾效果

--- src/myMVC/model/RandomQueue.java
+++ src/myMVC/model/RandomQueue.java
@@ -17,18 +17,22 @@ public class RandomQueue<E> {
             throw new IllegalArgumentException("There's no " +
                     "element to remove in Random Queue");
         }
+        // [0-queue.size())
         int randIndex = (int)(Math.random()*queue.size());
         E randomElement = queue.get(randIndex);
-        //
+        // 将最后一个元素赋给随机索引的位置
         queue.set(randIndex,queue.get(queue.size() - 1));
+        // 删除最后一个元素
         queue.remove(queue.size() - 1);
         return randomElement;
     }
 
+    // 随机队列剩余元素
     public int size(){
         return queue.size();
     }
 
+    // 随机队列是否为空
     public boolean empty(){
         return size() == 0;
     }
