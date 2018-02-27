2871ed701c96e52037a17e3784a8f50c32b7955a
aqie123 <2924811900@qq.com>
2018-02-28 07:38:56 +0800

    推箱子绘制界面完成

--- src/myMVC/app/App.java
+++ src/myMVC/app/App.java
@@ -5,7 +5,7 @@ import myMVC.controller.AlgoVisualizer;
 public class App {
     public static void main(String[] args) {
         // 模拟方块
-        String fileName = "src/resources/boston_09.txt";
+        java.lang.String fileName = "src/resources/boston_09.txt";
         AlgoVisualizer vis = new AlgoVisualizer(fileName);
     }
 }
